a0f8e9ede0faa30ee7dfd8b6e0ad3913d525f163
thzeribi <thzeribi@student.42.fr>
2020-01-14 01:51:50 +0100

    Finish operations.py

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -1,43 +1,74 @@
 import sys
 
+# Define Params List
 class Params:
-	def __init__(self, sum, diff, product, quotient, remaider):
+	def __init__(self, sum, diff, product, quotient, remainder):
 		self.sum = sum
 		self.diff = diff
 		self.product = product
 		self.quotient = quotient
 		self.remainder = remainder
 
-def error(nb):
+# Check and print error
+def print_error(nb):
 	if nb >= 3 or nb <= 0:
-		return "Usage: python operations.py\nExample:\n	python operations.py 10 3"
+		print("Usage: python operations.py\nExample:\n	python operations.py 10 3")
+		sys.exit()
 	error={
 			1: 'InputError: too many arguments\n',
 			2: 'InputError: only numbers\n'
 	}
-	return error.get(nb) + "Usage: python operations.py\nExample:\n	python operations.py 10 3"
+	print(error.get(nb) + "Usage: python operations.py\nExample:\n	python operations.py 10 3")
+	sys.exit()
 
+def get_error():
+	if len(sys.argv) <= 1:
+		print_error(0)
+	elif not sys.argv[1].isdigit() or not sys.argv[2].isdigit():
+		print_error(2)
+	elif len(sys.argv) >= 4:
+		print_error(1)
+	else:
+		operations(int(sys.argv[1]), int(sys.argv[2]))
+	return ;
+
+# Print Result
 def print_result(res):
-	return ;
+	print("Sum: " + str(res.sum))
+	print("Difference: " + str(res.diff))
+	print("Product: " + str(res.product))
+	print("Quotient: " + str(res.quotient))
+	print("Remainder: " + str(res.remainder))
+	sys.exit()
 
+# Calculate values
 def sum(a, b):
-	return ;
+	nbr = a + b
+	return nbr;
 
 def diff(a, b):
-	return ;
+	nbr = a - b
+	return nbr;
 
 def product(a, b):
-	return ;
+	nbr = a * b
+	return nbr;
 
 def quotient(a, b):
-	return ;
+	if b == 0:
+		return "ERROR (div by zero)"
+	nbr = a / b
+	return nbr;
 
 def remainder(a, b):
-	return ;
+	if b == 0:
+		return "ERROR (modulo by zero)"
+	nbr = a % b
+	return nbr;
 
 def operations(a, b):
+	res = Params(sum(a, b), diff(a,b), product(a, b), quotient(a, b), remainder(a ,b))
+	print_result(res)
 	return ;
 
-test = int(sys.argv[1])
-print(type(test))
-print(test)
+get_error()
