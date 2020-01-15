b581d100d016da630609e6d6b7aa2d8a80b4036c
thzeribi <thzeribi@student.42.fr>
2020-01-15 16:58:44 +0100

    Finish day00

--- day00/ex06/recipe.py
+++ day00/ex06/recipe.py
@@ -19,8 +19,8 @@ cookbook = {
 	},
 }
 
-# Type of recipes
-type = ['snack', 'steak', 'dish', 'dessert']
+# Types of recipes
+types = ['snack', 'breakfast', 'meal', 'dessert']
 
 # List for select arg in hud
 select_tab = []
@@ -37,29 +37,104 @@ def print_error(error_select, select, nb):
 	index = input(">> ")
 	return index;
 
+# Func for print cookbook
+def print_cookbook():
+	str = ""
+	for x in cookbook:
+		str += x
+		str += ", "
+	print("\nCookbook :\n" + str + "\n")
+	print_menu("main", 0)
 
-def testfunc():
-	print("yes")
-def testfunc2():
-	print("no")
+# Func for check and print recipe
+def print_recipe_check(recipe):
+	for x in cookbook:
+		if x == recipe:
+			return recipe;
+	recipe = print_error("print_recipe_no_exist", "print_recipe", 1)
+	recipe = print_recipe_check(recipe)
+	return recipe
+
+def print_recipe():
+	recipe = print_menu("print_recipe", 1)
+	recipe = print_recipe_check(recipe)
+	print("\nIngredients list: " + str(cookbook[recipe]['details']))
+	print("To be eaten for " + str(cookbook[recipe]['type']))
+	print("Takes " + str(cookbook[recipe]['time']) + " minutes of cooking.\n")
+	print_menu("main", 0)
+
+# Func for check en delete recipe
+def del_recipe_check(recipe):
+	for x in cookbook:
+		if recipe == x:
+			del cookbook[recipe]
+			return recipe;
+	recipe = print_error("del_recipe_no_exist", "print_recipe", 1)
+	recipe = print_recipe_check(recipe)
+	return recipe;
+
+def del_recipe():
+	recipe = print_menu("del_recipe", 1)
+	recipe = del_recipe_check(recipe)
+	print("\n" + recipe + " as been delete\n")
+	print_menu("main", 0)
 
 # Func for check recipe input
 def add_recipe_check(recipe):
 	for x in cookbook:
 		if recipe == x:
 			recipe = print_error("add_recipe_exist", "add_recipe", 1)
-			add_recipe_check(recipe)
-			sys.exit()
+			recipe = add_recipe_check(recipe)
+			return recipe;
 	if recipe == "exit":
 		exit()
 	return recipe
 
+def add_recipe_details(nb, list):
+	new = input("Add new ingredients, write stop for next step:\n>> ")
+	if new == "stop":
+		return list;
+	elif new == "exit":
+		print("You cant leave now !")
+	elif len(new) <= 0:
+		print("Enter a ingredients")
+		add_recipe_details(nb, list)
+	else:
+		list.insert(nb, new)
+		nb += 1
+		add_recipe_details(nb, list)
+	return list;
+
+def add_recipe_type():
+	type = input("Enter a type (snack, breakfast, meal, dessert)\n>> ")
+	if type in types:
+		return type;
+	else:
+		add_recipe_type()
+	return type;
+
+def add_recipe_time():
+	time = input("Enter cooking time for recipe\n>> ")
+	if len(time) <= 0 or  not time.isnumeric() or int(time) <= 0:
+		print("Not a valid time")
+		add_recipe_time()
+	else:
+		return time;
+	return time;
+
+
 # Func for add new recipes
 def add_recipe():
 	recipe = print_menu("add_recipe", 1)
-	add_recipe_check(recipe)
-	print(recipe + " new recipe !")
-	sys.exit()
+	recipe = add_recipe_check(recipe)
+	list = []
+	list = add_recipe_details(0, list)
+	type = add_recipe_type()
+	time = add_recipe_time()
+	new_dic = {recipe:{"details":list, "type":type, "time":time}}
+	cookbook.update(new_dic)
+	print("\nRecipe " + recipe + " as been add in cookbook !\n")
+	print_menu("main", 0)
 
 # Function for check en call specific function
 # in relation to the selected elements
@@ -112,16 +187,24 @@ def create_dic():
 		"main":{
 			"title":"Please select an option by typing the corresponding number:",
 			"Add recipe":add_recipe,
-			"Delete a recipe":testfunc,
-			"Print a recipe":testfunc,
-			"Print the cookbook":testfunc,
+			"Delete a recipe":del_recipe,
+			"Print a recipe":print_recipe,
+			"Print the cookbook":print_cookbook,
 			"Quit":exit
 		},
 		"add_recipe":{
-			"title":"\nPlease enter the recipe's name:",
+			"title":"\nPlease enter the recipe's name to add it:",
+		},
+		"del_recipe":{
+			"title":"\nPlease enter the recipe's name delete it:"
+		},
+		"print_recipe":{
+			"title":"\nPlease enter the recipe's name to get its details:"
 		},
 		"error":{
-			"add_recipe_exist":"\nRecipe already exist, retry please"
+			"add_recipe_exist":"\nRecipe already exist, please retry",
+			"print_recipe_no_exist":"\nYour recipe doesnt exist, please retry",
+			"del_recipe_no_exist":"\nYour recipe doesnt exist, please retry"
 		}
 	}
 	return hud;
